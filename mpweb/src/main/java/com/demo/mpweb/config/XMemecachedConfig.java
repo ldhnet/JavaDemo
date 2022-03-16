a2b87fd6482e04d3253f22c75069b66b39201e7c
ldhnet <574427343@qq.com>
2022-03-16 08:49:09 +0800

    redis demo

--- mpweb/src/main/java/com/demo/mpweb/config/XMemecachedConfig.java
+++ mpweb/src/main/java/com/demo/mpweb/config/XMemecachedConfig.java
@@ -1,28 +1,28 @@
-package com.demo.mpweb.config;
-
-import net.rubyeye.xmemcached.MemcachedClient;
-import net.rubyeye.xmemcached.MemcachedClientBuilder;
-import net.rubyeye.xmemcached.XMemcachedClientBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
-
-@Configuration
-public class XMemecachedConfig {
-
-    @Autowired
-    private  XMemcachedProperties memcachedProperties;
-
-    @Bean
-    public MemcachedClient getMemcachedClient() throws IOException {
-        MemcachedClientBuilder memcachedClientBuilder=new XMemcachedClientBuilder(memcachedProperties.getServers());
-
-        memcachedClientBuilder.setConnectionPoolSize(memcachedProperties.getPoolSize());
-        memcachedClientBuilder.setOpTimeout(memcachedProperties.getOpTimeout());
-
-        MemcachedClient memcachedClient=memcachedClientBuilder.build();
-        return  memcachedClient;
-    }
-}
+//package com.demo.mpweb.config;
+//
+//import net.rubyeye.xmemcached.MemcachedClient;
+//import net.rubyeye.xmemcached.MemcachedClientBuilder;
+//import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.io.IOException;
+//
+//@Configuration
+//public class XMemecachedConfig {
+//
+//    @Autowired
+//    private  XMemcachedProperties memcachedProperties;
+//
+//    @Bean
+//    public MemcachedClient getMemcachedClient() throws IOException {
+//        MemcachedClientBuilder memcachedClientBuilder=new XMemcachedClientBuilder(memcachedProperties.getServers());
+//
+//        memcachedClientBuilder.setConnectionPoolSize(memcachedProperties.getPoolSize());
+//        memcachedClientBuilder.setOpTimeout(memcachedProperties.getOpTimeout());
+//
+//        MemcachedClient memcachedClient=memcachedClientBuilder.build();
+//        return  memcachedClient;
+//    }
+//}
--- mpweb/src/main/java/com/demo/mpweb/service/impl/SMSCodeServiceImpl.java
+++ mpweb/src/main/java/com/demo/mpweb/service/impl/SMSCodeServiceImpl.java
@@ -3,8 +3,6 @@ package com.demo.mpweb.service.impl;
 import com.demo.mpweb.domain.SMSCode;
 import com.demo.mpweb.service.ISMSCodeService;
 import com.demo.mpweb.controller.ulits.CodeUtils;
-import net.rubyeye.xmemcached.MemcachedClient;
-import net.rubyeye.xmemcached.exception.MemcachedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
@@ -16,46 +14,46 @@ public class SMSCodeServiceImpl implements ISMSCodeService {
 
     @Autowired
     private CodeUtils codeUtils;
-    @Autowired
-    private MemcachedClient memcachedClient;
+//    @Autowired
+//    private MemcachedClient memcachedClient;
     /*
     获取验证码
      */
-//    @Override
-//    //@Cacheable(value = "smsCode",key = "#tele")
-//    @CachePut(value = "smsCode", key = "#tele")
-//    public String sendCodeToSMS(String tele) {
-//        String code = codeUtils.generator(tele);
-//        return code;
-//    }
-//
-//    @Override
-//    public Boolean checkCode(SMSCode smsCode) {
-//        String code = smsCode.getCode();
-//        String codeCache = codeUtils.get(smsCode.getTele());
-//        return code.equals(codeCache);
-//    }
-
     @Override
+    //@Cacheable(value = "smsCode",key = "#tele")
+    @CachePut(value = "smsCode", key = "#tele")
     public String sendCodeToSMS(String tele) {
         String code = codeUtils.generator(tele);
-        try {
-            memcachedClient.set(tele,10,code);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         return code;
     }
 
     @Override
     public Boolean checkCode(SMSCode smsCode) {
-        String codeCache=null;
-        try {
-            codeCache = memcachedClient.get(smsCode.getTele()).toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return smsCode.getCode().equals(codeCache);
+        String code = smsCode.getCode();
+        String codeCache = codeUtils.get(smsCode.getTele());
+        return code.equals(codeCache);
     }
+
+//    @Override
+//    public String sendCodeToSMS(String tele) {
+//        String code = codeUtils.generator(tele);
+//        try {
+//            memcachedClient.set(tele,10,code);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return code;
+//    }
+//
+//    @Override
+//    public Boolean checkCode(SMSCode smsCode) {
+//        String codeCache=null;
+//        try {
+//            codeCache = memcachedClient.get(smsCode.getTele()).toString();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return smsCode.getCode().equals(codeCache);
+//    }
 }
